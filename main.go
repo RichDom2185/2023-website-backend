1f09c8e306811b2b9384e64936e1ed98a191eb80
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-08-20 16:11:43 +0800

    Refactor router to separate package

--- main.go
+++ main.go
@@ -6,24 +6,14 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/RichDom2185/2023-website-backend/handlers"
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
-	"github.com/go-chi/cors"
+	"github.com/RichDom2185/2023-website-backend/router"
 	"github.com/joho/godotenv"
 )
 
 func main() {
 	godotenv.Load()
 
-	r := chi.NewRouter()
-	r.Use(middleware.Logger)
-	r.Use(cors.Handler(cors.Options{
-		AllowedOrigins: []string{"https://*", "http://*"},
-		AllowedMethods: []string{"GET", "POST", "OPTIONS"},
-	}))
-	r.Get("/", handlers.HandleHealthCheck)
-	r.Post("/resume", handlers.HandleResumeForm)
+	r := router.Setup()
 
 	appMode := os.Getenv("GO_ENV")
 	if appMode == "" {
---
+++ router/router.go
@@ -0,0 +1,21 @@
+package router
+
+import (
+	"github.com/RichDom2185/2023-website-backend/handlers"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+)
+
+func Setup() chi.Router {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins: []string{"https://*", "http://*"},
+		AllowedMethods: []string{"GET", "POST", "OPTIONS"},
+	}))
+	r.Get("/", handlers.HandleHealthCheck)
+	r.Post("/resume", handlers.HandleResumeForm)
+
+	return r
+}
