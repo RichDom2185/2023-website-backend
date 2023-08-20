2273593118bc7e93a55b37e90e2e93c3cbcebd89
Richard Dominick <34370238+RichDom2185@users.noreply.github.com>
2023-08-20 17:00:35 +0800

    Allow passing custom middleware to router

--- router/router.go
+++ router/router.go
@@ -1,19 +1,25 @@
 package router
 
 import (
+	"net/http"
+
 	"github.com/RichDom2185/2023-website-backend/handlers"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/cors"
 )
 
-func Setup() chi.Router {
+func Setup(customMiddleware ...func(http.Handler) http.Handler) chi.Router {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Use(cors.Handler(cors.Options{
 		AllowedOrigins: []string{"https://*", "http://*"},
 		AllowedMethods: []string{"GET", "POST", "OPTIONS"},
 	}))
+
+	for _, m := range customMiddleware {
+		r.Use(m)
+	}
 
 	setupRoutes(r)
 	return r
